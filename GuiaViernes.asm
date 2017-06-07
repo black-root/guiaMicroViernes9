ff32468d5df86ad584ea770b1505520cde953a2e
Ronald Ibañez <ronald_reir21@hotmail.com>
2017-06-06 23:54:01 -0600

    se agrego el ejercicio 3, funcional

--- GuiaViernes.asm
+++ GuiaViernes.asm
@@ -6,11 +6,12 @@
   
   msj1 db 'El numero de palabras es:$'
   msj2 db 'Ingrese una cadena:$'  
-  msj3 db 'La cantidad de numeros es:$'
+  msj3 db 'La cantidad de numeros es:$'    
+  msj4 db 'La cantidad de letras es:$'
   
   palabra db ?
   contNum db ?
-  contLetra db ?, ? 
+  contLetra db ?, ?, ? 
   
  
   
@@ -41,8 +42,8 @@ menu macro
      cmp cx, 2
      je call case2 
      
-     cmp cx 3
-     je call case 3
+     cmp cx,3
+     je call case3
      
      cmp cx, 9
      je call fin
@@ -89,17 +90,28 @@ llenado proc
         mov [SI],al; copia el contenido de AL en el registro cuya direccion es igual al contenido de SI
         inc SI     ; Incrementa en 1 el contenido de SI
         inc bx     ;contador de palabra 
-        call palabras
         
-        sigue:
+        call palabras
+        sigue:     
+        
         call numeros
         sigue1:
+        
+        call letrasMa
+        sigue2:    
+        
+        call letrasMi
+        sigue3:
+        
         mov bl, palabra[0] 
+       
         mov dl,al  ; compia el contenido de dl en al 
         mov ah,02h ; Funcion de mostrar por pantalla el contenido de dl 
-        int 21h    ; ejecuta la funcion del DOS           
-        ;add palabra[0],1
-        add bl, 1 
+        int 21h    ; ejecuta la funcion del DOS   
+        add dh, contLetra[0] 
+        add dh, contLetra[1] 
+        mov contLetra[2], dh
+        mov dh, 0       
         
         loop regresa ; En contenido de CX disminuye en 1 y salta a regresa  
     
@@ -134,23 +146,23 @@ numeros proc
     ret
 numeros endp
 
-letrasM proc
+letrasMa proc
     cmp al, 65
     jb sigue2
     cmp al, 90
     ja sigue2       
     add contLetra[0],1
     ret
-letrasM endp
+letrasMa endp
 
-letrasm proc
+letrasMi proc
     cmp al, 97
     jb sigue3
     cmp al, 122
     ja sigue3       
     add contLetra[1],1
     ret
-letrasm proc
+letrasMi endp
 
 limpiar proc
     MOV AX,0600H ; Peticion para limpiar pantalla  
@@ -288,7 +300,60 @@ case2 proc
     ret
 case2 endp    
 
-case3 proc
+case3 proc  
+     call limpiar   
+     
+     mov bl, contLetra[2]
+    
+     mov al,bl
+     and ax,000fh
+     and bx,00f0h
+     shr bx,01
+     mov ah,bl
+     cmp al,0ah
+     jb dejar1
+     daa
+     inc ah
+     
+     dejar3:
+     mov bl,al
+     mov al,ah
+     cmp al,0ah
+     jb decena3
+     daa
+     mov dx,31h
+     
+     decena3:
+     mov bh,al
+     and bx,0f0fh
+     or bx,3030h
+     mov cx,bx
+            
+     mov ah,02
+     mov dx,0702h
+     mov bh,00
+     int 10h
+    
+     mov ah,09 ;    Escribir cadena
+     mov dx,offset msj4 ;cantidad de letras
+     int 21h
+     
+     mov ah,02
+     mov dx,071eh
+     mov bh,00
+     int 10h
+     
+     mov dl,ch
+     mov dh,cl
+     
+     mov ah,02
+     mov cx,01
+     int 21h
+     mov ch,dh
+     mov dl,ch
+     int 21h 
+     
+    jmp regre
     
     ret
 case3 endp                
